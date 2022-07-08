650b93fbb96ba0a5ba542916f7ca391a5c160746
siyual-park <siyual.bak@gmail.com>
2022-07-08 12:18:26 +0900

    feat: broadcast

--- src/handler/connect.handler.ts
+++ src/handler/connect.handler.ts
@@ -2,12 +2,15 @@ import { Server, Socket } from 'socket.io';
 
 import join from './join.handler';
 import leave from './leave.handler';
+import broadcast from './broadcast.handler';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function connect(io: Server) {
   return (socket: Socket) => {
     socket.on('join-room', join(socket));
     socket.on('leave-room', leave(socket));
+
+    socket.on('broadcast', broadcast(socket));
   };
 }
 
---
+++ test/broadcast.spec.ts
@@ -0,0 +1,42 @@
+import { faker } from '@faker-js/faker';
+
+import createClient from './create-client';
+import createServer from './create-server';
+import wrap from './wrap';
+
+describe('broadcast', () => {
+  test('room', wrap(async (done) => {
+    const [, config] = await createServer();
+
+    const socket1 = await createClient(config);
+    const socket2 = await createClient(config);
+
+    const randomRoom = faker.name.findName();
+
+    socket1.on('hello', (arg) => {
+      expect(arg).toEqual('world');
+
+      done();
+    });
+
+    socket1.emit('join-room', randomRoom);
+    socket2.emit('join-room', randomRoom);
+
+    socket2.emit('broadcast', randomRoom, 'hello', 'world');
+  }), 1000);
+
+  test('all', wrap(async (done) => {
+    const [, config] = await createServer();
+
+    const socket1 = await createClient(config);
+    const socket2 = await createClient(config);
+
+    socket1.on('hello', (arg) => {
+      expect(arg).toEqual('world');
+
+      done();
+    });
+
+    socket2.emit('broadcast', null, 'hello', 'world');
+  }), 1000);
+});
